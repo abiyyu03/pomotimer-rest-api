75f6b41dc90519964f46ac2ba24e0c4f59058757
abiyyu03 <abiyyu652@gmail.com>
2022-09-12 15:02:35 +0700

    add store user data (registration)

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,15 +2,39 @@ const express = require('express');
 const {nanoid} = require('nanoid');
 // const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
+const {userSchema,user} = require('../models/userSchema');
 
-const registration = () => {
+const registration = async (req,res) => {
+    const { username, email, password} = req.body;
+    const id = 123;
+    const createdAt = new Date().getTime();
+    const updatedAt = new Date().getTime();
+    
+    const savedUser = new user({
+        _userId:id,
+        username:username,
+        email:email,
+        password:password,
+        createdAt:createdAt,
+        updatedAt:updatedAt,
+    });
+    await savedUser.save();
+    res.status(201).json({
+        status:'success',
+        message:"Data stored successfully",
+    });
+}
 
+const getAllUserData = async (req, res) => {
+    await res.json(await user.find());
 }
 
 const login = () => {
-
+    
 }
-// module.exports = {
-//     login,
-//     register,
-// };
+
+module.exports = {
+    login,
+    registration,
+    getAllUserData
+};
