a9adb24fd956f3d036802de1eea88a5870f160f9
abiyyu03 <abiyyu652@gmail.com>
2022-09-12 11:42:36 +0700

    remove unused library

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const cors = require('cors');
 const request = require('request');
 const urlParse = require('url-parse');
 const queryParse = require('query-string');
-const { auth } = require('express-openid-connect');
-const expressSession = require('express-session');
-const passport = require('passport');
-const auth0Strategy = require('passport-auth0');
+// const { auth } = require('express-openid-connect');
+// const expressSession = require('express-session');
+// const passport = require('passport');
+// const auth0Strategy = require('passport-auth0');
 
 const { dbHost, dbPort, dbName, dbUsername, dbPassword } = require('./config/database');
 const config = require('./config/auth');
@@ -21,7 +21,7 @@ const port = process.env.PORT || 5000;
 const host = process.env.HOST;
 
 app.use(cors());
-app.use(auth(config));
+// app.use(auth(config));
 app.use(express.json()); //parse JSON
 app.use(bodyParser.urlencoded({extended: true})); // parse JSON from form html
 app.use(require('./routes/routes')); //get route
