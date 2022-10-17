66e3433feaa40aef30117f8d5eb997c21b4b00f8
abiyyu03 <abiyyu652@gmail.com>
2022-10-17 14:22:04 +0700

    update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const router = express.Router();
 
-const {userStatisticSchema,userStatistic} = require('../models/userStatisticSchema');
-const {userSchema,user} = require('../models/userSchema');
+// const {userStatisticSchema,userStatistic} = require('../models/userStatisticSchema');
+// const {userSchema,user} = require('../models/userSchema');
 
 const {storeDataAtStart,storeDataAtEnd,getAllUserStatisticData,deleteUserStatisticData,getByIdUserStatisticData} = require('../controllers/UserStatisticController');
 const {login,registration,getAllUserData} = require('../controllers/UserController');
+const {addTodolistData, getAllTodolistData, deleteTodolistData, getByIdTodolistData} = require('../controllers/TodolistController');
+const {getAllTaskData, addTaskData, getByIdTaskData} = require('../controllers/TaskController');
 
 //route area
-  
+router.get('/tasks', async (req,res) => {
+    await getAllTaskData(req,res);
+});
+
+router.post('/tasks', async (req,res) => {
+    await addTaskData(req,res);
+});
+
+router.get('/tasks/:id', async (req,res) => {
+	await getByIdTaskData(req, res);
+});
+
+router.get('/todolists', async (req,res) => {
+    await getAllTodolistData(req,res);
+});
+
+router.post('/todolists', async (req,res) => {
+    await addTodolistData(req,res);
+});
+
+router.delete('/todolists/:id', async (req,res) => {
+    await deleteTodolistData(req,res);
+});
+
+router.get('/todolists/:id', async (req,res) => {
+    await getByIdTodolistData(req,res);
+});
+
 router.post('/register', async (req,res) => {
     await registration(req,res);
 });
